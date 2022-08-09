121cfc7b0503c456d1ea52d3a5866a812d091b95
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-08-09 17:33:13 +0200

    References and the borrow checker

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let mut whom = "world";
+    let mut whom: &str = "world";
     println!("Hello, {whom}!");
     whom = "planet";
     println!("Bye, {whom}!");
+    let y: &str;
+    let x = String::from("This is not a &str");
+    y = &x;
+    use_string(&x);
+    println!("x = {x}");
+    println!("y = {y}");
+}
+
+fn use_string(s: &str) {
+    println!("Inside of use_string: s = {s}");
 }
