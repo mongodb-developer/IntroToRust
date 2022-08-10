173c8a7f9d5fed419c684ef18375419e5b72fc3d
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-08-10 10:50:06 +0200

    Define Task type

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
+mod task;
+
+use task::Task;
+
 fn main() {
-    let mut whom: &str = "world";
-    println!("Hello, {whom}!");
-    whom = "planet";
-    println!("Bye, {whom}!");
-    let x = String::from("This is not a &str");
-    use_string(&x);
-    println!("x = {x}");
-
-    let times = "5";
-    let times: Option<i32> = times.parse().ok();
-    println!("times = {times:?}");
-    if let Some(v) = times {
-        println!("times = {v}");
-    }
-}
-
-fn use_string(s: &str) {
-    println!("Inside of use_string: s = {s}");
+    let task = Task {};
+    println!("Task: {task:?}");
 }
