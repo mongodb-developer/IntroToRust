6ec6280205ed98921f5d3e6caaeeb1c195b61b49
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-08-11 13:51:36 +0200

    Use traits

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ mod in_memory_task_repo;
 mod task;
 mod task_repo;
 
+use std::fmt::Debug;
+
 use task::Task;
 
 use crate::in_memory_task_repo::InMemoryTaskRepo;
 use crate::task_repo::TaskRepo;
 
 fn main() {
+    let repo = InMemoryTaskRepo::new();
+    use_task_repo(repo);
+}
+
+fn use_task_repo<T: TaskRepo + Debug>(mut repo: T) {
     let task = Task::new("Do homework".to_string(), None, false);
     println!("Task: {task:?}");
-    let mut repo = InMemoryTaskRepo::new();
     repo.add(task);
     println!("Repo: {repo:?}");
     println!("Tasks in repo: {:?}", repo.list())
